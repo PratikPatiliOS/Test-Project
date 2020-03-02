d63cb2f8e1987b63ed1d5e2fe56f09dd545bf0bc
Pratk Patil <contactme.pratikpatil@gmail.com>
2020-03-02 10:38:24 +0530

    first commit 2nd March 2020

---
+++ TestApp/NetworkLayer/Manager/NetworkManager.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkManager.swift
+//  TestApp
+//
+//  Created by Amol Chaudhari on 01/03/20.
+//  Copyright © 2020 Pratik Patil. All rights reserved.
+//
+
+import Foundation
---
+++ TestApp/Utility/Extensions/ImageView.swift
@@ -0,0 +1,9 @@
+//
+//  ImageView.swift
+//  TestApp
+//
+//  Created by Amol Chaudhari on 01/03/20.
+//  Copyright © 2020 Pratik Patil. All rights reserved.
+//
+
+import Foundation
